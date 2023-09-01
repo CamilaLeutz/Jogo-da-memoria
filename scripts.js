fa3bc0e0d50bed2d224d0e3349c983ee15936e2e
camilaleutz <leutz1989@gmail.com>
2023-09-01 06:03:45 -0300

    Funções funcionando, localStorage rodando, e pagina login sendo direcionada a pagina do jogo

--- scripts.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector(".login-input")
-const button = document.querySelector(".login-button")
-
-const validateInput = (event) => {
-    console.log(event.target.value)
-}
-input.addEventListener("input", validateInput)
